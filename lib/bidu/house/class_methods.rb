4c7dc161a8a2d49e07f29f972c31ee73ee99ba8d
darthjee favini <darthjee@gmail.com>
2015-07-06 00:40:22 -0300

    Extract id from params

--- lib/bidu/house/class_methods.rb
+++ lib/bidu/house/class_methods.rb
@@ -1,9 +1,11 @@
 module Bidu::House::ClassMethods
 
   def status_report(*attr_names)
+    id = attr_names.first
     options = {
+      id: id
     }.merge(attr_names.extract_options!)
 
-    self.status_builder.add_report_config(attr_names.first, options)
+    self.status_builder.add_report_config(id, options)
   end
 end
