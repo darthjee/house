5de0f558769c77e88b30b5d8ec9b2fd5fcc5da07
darthjee favini <darthjee@gmail.com>
2015-07-06 00:44:03 -0300

    Move class methods to class methods

--- lib/bidu/house/class_methods.rb
+++ lib/bidu/house/class_methods.rb
@@ -8,4 +8,8 @@ module Bidu::House::ClassMethods
 
     self.status_builder.add_report_config(id, options)
   end
+
+  def status_builder
+    @status_builder ||= Bidu::House::StatusBuilder.new
+  end
 end
--- lib/bidu/house/concern.rb
+++ lib/bidu/house/concern.rb
@@ -2,17 +2,9 @@ module Bidu
   module House
     extend ActiveSupport::Concern
 
-    included do
-      class << self
-        def status_builder
-          @status_builder ||= Bidu::House::StatusBuilder.new
-        end
-      end
-
-      def render_status(key)
-        status = self.class.status_builder.build(key, params)
-        render json: status.as_json, status: status.status
-      end
+    def render_status(key)
+      status = self.class.status_builder.build(key, params)
+      render json: status.as_json, status: status.status
     end
   end
 end
