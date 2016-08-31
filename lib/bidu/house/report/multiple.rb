ba968a07b6fa15ee51521ffc2f7b4b8f78fdf616
darthjee <darthjee@gmail.com>
2016-08-31 11:54:38 -0300

    Add Multiple report

---
+++ lib/bidu/house/report/multiple.rb
@@ -0,0 +1,29 @@
+class Bidu::House::Report::Multiple < Bidu::House::Report
+  def as_json
+    {
+      status: status
+    }.merge(sub_reports_hash)
+  end
+
+  def error?
+    sub_reports.any?(&:error?)
+  end
+
+  private
+
+  def sub_reports_hash
+    sub_reports.map(&:as_json).as_hash(reports_ids.map(&:to_s))
+  end
+
+  def sub_reports
+    @sub_reports ||= reports_ids.map do |id|
+      build_sub_report(id)
+    end
+  end
+
+  def build_sub_report(id)
+    sub_report_class.new(json.merge(key => id))
+  end
+end
+
+
