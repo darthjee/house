4fe9d4deb34c4df585525e1fd51ea6a8723bbe46
darthjee <darthjee@gmail.com>
2016-10-27 02:05:32 -0200

    Avoid second fluturation

--- lib/bidu/house/at.rb
+++ lib/bidu/house/at.rb
@@ -2,6 +2,15 @@ class Bidu::House::At
   include ActiveModel::Model
   attr_accessor :year, :month, :day, :week_day, :hour, :minute, :second
 
+  def initialize(*args)
+    super(*args)
+    @month = 0 if year && !month
+    @day = 0 if month && !day
+    @hour = 0 if day && !hour
+    @minute = 0 if hour && !minute
+    @second = 0 if minute && !second
+  end
+
   def >(target)
     time(target) > target
   end
