c9810710d65059e7f5db97b67b74604c3e73b895
darthjee <darthjee@gmail.com>
2018-06-01 17:11:33 +0200

    Add TypeCast#to_infinity_float

--- lib/json_parser/type_cast_ext.rb
+++ lib/json_parser/type_cast_ext.rb
@@ -3,5 +3,12 @@ module JsonParser
     def to_period(value)
       Mercy::PeriodParser.parse(value)
     end
+
+    def to_infinity_float(value)
+      return -Float::INFINITY if value.match(/-inf/)
+      return Float::INFINITY if value.match(/inf/)
+
+      value.to_f
+    end
   end
 end
--- spec/lib/json_parser/type_cast_spec.rb
+++ spec/lib/json_parser/type_cast_spec.rb
@@ -5,7 +5,7 @@ describe JsonParser::TypeCast do
 
   let(:dummy_class) { Class.new { include JsonParser::TypeCast } }
 
-  describe '.to_period' do
+  describe '#to_period' do
     it_behaves_like 'a method that knows how to parse time', :to_period, {
       '3' => 3.seconds,
       '3seconds' => 3.seconds,
@@ -15,5 +15,43 @@ describe JsonParser::TypeCast do
       '3months' => 3.months,
       '3years' => 3.years
     }
+  end
+
+  describe '#to_infinity_float' do
+    context 'when value can be converted to integer' do
+      let(:value) { (Random.rand * 201).to_i - 100 }
+
+      it 'converts to integer' do
+        expect(subject.to_infinity_float(value.to_s)).to eq(value)
+      end
+
+      it do
+        expect(subject.to_infinity_float(value.to_s)).to be_a(Float)
+      end
+    end
+
+    context 'when value is inf' do
+      let(:value) { 'inf' }
+
+      it 'converts to integer' do
+        expect(subject.to_infinity_float(value)).to eq(1 / 0.0)
+      end
+
+      it do
+        expect(subject.to_infinity_float(value)).to be_a(Float)
+      end
+    end
+
+    context 'when value is -inf' do
+      let(:value) { '-inf' }
+
+      it 'converts to integer' do
+        expect(subject.to_infinity_float(value)).to eq(-1 / 0.0)
+      end
+
+      it do
+        expect(subject.to_infinity_float(value)).to be_a(Float)
+      end
+    end
   end
 end
