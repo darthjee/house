86f2c3fdf4c8fce118fc023a40efa3895ec5d92b
darthjee <darthjee@gmail.com>
2019-05-07 19:13:53 +0200

    Add missing scripts

--- scripts/check_readme.sh
+++ scripts/check_readme.sh
@@ -3,4 +3,4 @@
 PROJECT=mercy
 VERSION=$(grep VERSION lib/$PROJECT/version.rb  | sed -e "s/.*'\(.*\)'/\1/g")
 
-echo grep https://www.rubydoc.info/gems/$PROJECT/$VERSION README.md
+grep https://www.rubydoc.info/gems/$PROJECT/$VERSION README.md
